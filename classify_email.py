9682227e972a297e0eb114a254cf32642c10b9cb
AswinswwdfdsfdKumar <2301678E@student.tp.edu.sg>
2024-08-07 22:24:12 +0800

    Add files via upload

--- classify_email.py
+++ classify_email.py
@@ -57,18 +57,7 @@ def classify_email(text):
 
 # Main function for Streamlit app
 def main():
-    st.markdown(
-        """
-        <style>
-        .reportview-container {
-            background-color: #e0f7fa;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-
-    st.title('Email Inquiry Classifier')
+    st.title('Customer Service Bot')
 
     # Text input for the inquiry
     inquiry = st.text_input("Enter your inquiry:")
